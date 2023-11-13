f4f57732b72106b85407af1afa27c4f44975462e
Felix Watson <felix.dork@gmail.com>
2023-11-13 22:40:06 +1100

    new test cases

--- test/ParseMidi.purs
+++ test/ParseMidi.purs
@@ -6,97 +6,68 @@ import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
-import ParseMidi (Event(..), HeaderEvent(..), MetaEvent(..), MidiEvent(..), SmpteOffset, parseEvent, parseFile, parseHeader, parseLenBytes, parseMidi, parseNoteOff)
+import ParseMidi (Event(..), HeaderEvent(..), parseFile, parseFileHeader, parseHeader, parseTrackHeader, parseVarLenNum)
 import Test.Unit (suite, test)
 import Test.Unit.Assert as Assert
 import Test.Unit.Main (runTest)
 
-midiParserTests :: Effect Unit
-midiParserTests = do
+individualParserTests :: Effect Unit
+individualParserTests = do
     runTest do
-        suite "parseNoteOff - MIDI event parser" do
-            test "correctly read valid input" do
+        suite "parse variable length" do
+            test "parseVarLenNum: read single number" do
                 let
-                    expected = Right $ Tuple (MidiEvent $ NoteOff {key: 1, vel: 1, chan: 1}) [0,0,0]
-                    input = [129, 1, 1, 0,0,0]
-                Assert.equal expected (parseNoteOff input)
-            test "Nothing if input has insufficient bytes" do
+                    expected = Right $ Tuple 1 []
+                    input = [ 1 ]
+                Assert.equal expected (parseVarLenNum input)
+            test "parseVarLenNum: read longer number" do
                 let
-                    expected = Left "failed at noteOff"
-                    input = [129, 1]
-                Assert.equal expected (parseNoteOff input)
-
-majorMidiParseTests :: Effect Unit
-majorMidiParseTests = do
-    runTest do
-        suite "MidiEvent parser tests" do
-            test "correctly ident note off" do
+                    expected = Right $ Tuple 0x3FFF []
+                    input = [ 0xff, 0x7F ]
+                Assert.equal expected (parseVarLenNum input)
+            test "parseVarLenNum: read longest number" do
                 let
-                    expected = Right $ Tuple (MidiEvent $ NoteOff {key: 1, vel: 1, chan: 1}) [0,0,0]
-                    input = [129, 1, 1, 0,0,0]
-                Assert.equal expected (parseMidi input)
-            test "correctly ident note on" do
+                    expected = Right $ Tuple 0xFFFFFFF []
+                    input = [ 0xff, 0xff, 0xff, 0x7F ]
+                Assert.equal expected (parseVarLenNum input)
+            test "parseVarLenNum: should fail if illegal length" do
                 let
-                    expected = Right $ Tuple (MidiEvent $ NoteOn {key: 1, vel: 1, chan: 1}) [0,0,0]
-                    input = [145, 1, 1, 0,0,0]
-                Assert.equal expected (parseMidi input)
-            test "correctly ident polykeypress" do
+                    expected = Left "variable length exceeded allowed length"
+                    input = [ 0xff, 0xff, 0xff, 0xff, 0xff ]
+                Assert.equal expected (parseVarLenNum input)
+            test "parseFileHeader: read valid file header chunk" do
                 let
-                    expected = Right $ Tuple (MidiEvent $ PolyKeyPress) [0,0,0]
-                    input = [160, 1, 1, 0,0,0]
-                Assert.equal expected (parseMidi input)
-            test "correctly ident progChange" do
+                    expected = Just $ Tuple (HeaderEvent $ File {format: 1, nTracks: 332, division: 480}) []
+                    input = [ 0, 0, 0, 6, 0, 1, 1, 76, 1, 224 ] 
+                Assert.equal expected (parseFileHeader input)
+            test "parseFileHeader: read invalid header chunk" do
                 let
-                    expected = Right $ Tuple (MidiEvent $ ProgChange) []
-                    input = [193, 1]
-                Assert.equal expected (parseMidi input)
-            test "test out parseLenBytes" do
+                    expected = Nothing
+                    input = [ 0, 0, 0, 6, 0, 1, 1, 76, 129, 224 ] 
+                Assert.equal expected (parseFileHeader input)
+            test "parseTrackHeader: read valid track header chunk" do
                 let
-                    expected = Just $ Tuple 259 [0,0,0]
-                    input = [129, 129, 1, 0,0,0]
-                Assert.equal expected (parseLenBytes 0 input)
-
-eventParserTests :: Effect Unit
-eventParserTests = do
-    runTest do
-        suite "Event parser Tests" do
-            test "correctly ident track header" do
+                    expected = Just $ Tuple (HeaderEvent $ Track 2250) [0, 255]
+                    input = [0, 0, 8, 202, 0, 255] 
+                Assert.equal expected (parseTrackHeader input)
+            test "parseHeader: read entire valid header chunk using generic parseHeader" do
                 let
-                    expected = Right $ Tuple (HeaderEvent $ File) []
-                    input = [77, 84, 104, 100,0,0, 0, 0]
-                Assert.equal expected (parseEvent input)
-            test "test parse text event" do
+                    expected = Right $ Tuple (HeaderEvent $ Track 2250) [0, 255]
+                    input = [ 114, 107, 0, 0, 8, 202, 0, 255] 
+                Assert.equal expected (parseHeader input)
+            test "parseHeader: read entire valid file header chunk using generic parseHeader" do
                 let
-                    expected = Right $ Tuple (MetaEvent $ Text "MMM") []
-                    input = [0, 255, 1, 3, 77, 77,77]
-                Assert.equal expected (parseEvent input)
-            test "correctly ident progChange" do
+                    expected = Right $ Tuple (HeaderEvent $ File {format: 1, nTracks: 332, division: 480}) [77, 84]
+                    input = [ 104, 100, 0, 0, 0, 6, 0, 1, 1, 76, 1, 224, 77, 84] 
+                Assert.equal expected (parseHeader input)
+            test "parseHeader: should fail if header invalid" do
                 let
-                    expected = Right $ Tuple (MidiEvent $ ProgChange) [0, 255, 0, 0]
-                    input = [193, 1, 0, 255, 0, 0]
-                Assert.equal expected (parseEvent input)
-            test "correctly ident chanPress" do
+                    expected = Left "Failed to reader Header Chunk"
+                    input = [ 104, 100, 0, 0, 0, 6, 1, 1, 76, 1, 224, 77, 84] 
+                Assert.equal expected (parseHeader input)
+            test "parseFile: read opening of valid test file" do
                 let
-                    expected = Right $ Tuple (MidiEvent $ AfterTouch) [0, 255, 0, 0]
-                    input = [208, 1, 0, 255, 0, 0]
-                Assert.equal expected (parseEvent input)
-            test "correctly do SMTPE offset" do
-                let
-                    expected = Right $ Tuple (MetaEvent $ SmpteOffset {hr: 65, mn: 0, sec: 30, fr: 2, fFr: 55}) [] 
-                    input = [0, 255, 84, 5,65, 0,30,2,55]
-                Assert.equal expected (parseEvent input)
-            test "correctly do tempo" do
-                let
-                    expected = Right $ Tuple (MetaEvent $ Tempo 0) [] 
-                    input = [0, 255, 81, 3,15, 0,15,200,242, 158]
-                Assert.equal expected (parseEvent input)
-
-parseFileTests :: Effect Unit
-parseFileTests = do
-    runTest do
-        suite "file parse tests" do
-            test "tiny file test" do
-                let
-                    expected = Right [(HeaderEvent File),(MetaEvent (Text "MMM")),(MidiEvent ProgChange)]
-                    input = [77, 84, 104, 100, 0, 0, 0, 6, 0, 1, 1, 75,1, 224, 0, 255, 1, 3, 77, 77, 77, 193, 1]
+                    expected = Left "ldkfj"
+                    input = [ 77, 84, 104, 100, 0, 0, 0, 6, 1, 1, 76, 1, 224, 77, 84, 114, 107, 0,0,8,202] 
                 Assert.equal expected (parseFile input)
+
