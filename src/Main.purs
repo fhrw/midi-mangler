6c809748282c7fd2cb258b1769b84f3a2e33bedd
Felix Watson <felix.dork@gmail.com>
2024-01-19 16:04:40 +1100

    built basic midi opening functionality

--- src/Main.purs
+++ src/Main.purs
@@ -2,60 +2,84 @@ module Main where
 
 import Prelude
 
-import Data.Maybe (Maybe(..), maybe)
+import DOM.HTML.Indexed.InputAcceptType (InputAcceptType(..), InputAcceptTypeAtom(..))
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Tuple (Tuple(..))
 import Effect (Effect)
-import Effect.Class (class MonadEffect)
+import Effect.Aff.Class (class MonadAff, liftAff)
+import Effect.Aff.Compat (EffectFnAff, fromEffectFnAff)
+import Effect.Class.Console (log)
 import Halogen as H
 import Halogen.Aff (awaitBody, runHalogenAff)
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
 import Halogen.VDom.Driver (runUI)
-import Node.Buffer (toArray)
-import Node.FS.Sync (readFile)
-import ParseMidi (parseFile)
+import ParseMidi (MidiFile, parseFile)
+import Web.Event.Event (Event)
 
-type State = Maybe String
+type State = { mMidiFile :: Maybe MidiFile
+             }
 
-data Action = GodSpeed
+data Action = GodSpeed Event
 
 main :: Effect Unit
 main = runHalogenAff do
-  body <- awaitBody
-  runUI component unit body
+    body <- awaitBody
+    runUI component unit body
 
-component :: forall query input output m. MonadEffect m => H.Component query input output m
+component :: forall query input output m. MonadAff m => H.Component query input output m
 component =
-  H.mkComponent
-    { initialState
-    , render
-    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
-    }
+    H.mkComponent
+        { initialState
+        , render
+        , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
+        }
 
 initialState :: forall input. input -> State
-initialState _ = Nothing
+initialState _ = { mMidiFile: Nothing}
 
 render :: forall m. State -> H.ComponentHTML Action () m
 render state = do
-        let val = maybe "file not opened yet" show state
-        HH.div_
-                [ HH.h1_
-                        [HH.text "MIDI Mangler"]        
-                , HH.p_
-                        [ HH.text ("file date: " <> val)]
-                , HH.button
-                        [HE.onClick \_ -> GodSpeed ]
-                        [ HH.text "Open file"]
-                ]
+    HH.div_
+        [ HH.h1_
+              [ HH.text "MIDI Mangler" ]
+        , HH.p_
+              [ HH.text
+                    $ case state.mMidiFile of
+                          Nothing -> ""
+                          Just _ -> "Loaded file"
+              ]
+        , HH.form_
+              [ HH.input
+                    [ HP.type_ HP.InputFile
+                    , HP.accept $ InputAcceptType [ AcceptFileExtension ".mid" ]
+                    , HE.onChange \event -> GodSpeed event
+                    ]
+              ]
+        ]
 
-handleAction :: forall output m. MonadEffect m => Action -> H.HalogenM State Action () output m Unit
+handleAction :: forall output m. MonadAff m => Action -> H.HalogenM State Action () output m Unit
 handleAction = case _ of
-        GodSpeed -> do
-                file <- H.liftEffect openMidi
-                H.modify_ \_ -> Just file
+    GodSpeed event -> do
+        mFile <- liftAff
+            $ fromEffectFnAff
+            $ readFileFromFilePickEvent
+                  { just: Just
+                  , nothing: Nothing
+                  , event
+                  }
+        let res = parseFile $ fromMaybe [] mFile
+        case res of
+                Left _ -> pure unit
+                Right (Tuple midi _) -> do 
+                        log $ show midi
+                        H.modify_ \st -> st {mMidiFile = Just midi}
 
-openMidi :: Effect String
-openMidi = do
-        buf <- readFile "./1m1.mid"
-        arr <- toArray buf
-        pure $ show $ parseFile arr
-
+foreign import readFileFromFilePickEvent
+    :: { just :: forall a. a -> Maybe a
+       , nothing :: forall a. Maybe a
+       , event :: Event
+       }
+    -> EffectFnAff (Maybe (Array Int))
