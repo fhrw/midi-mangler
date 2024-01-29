6dea7165faffc6e9ef99c9bc1081aa1b25532821
Felix Watson <felix.dork@gmail.com>
2024-01-29 17:24:20 +1100

    wrote func to fetch track notes from istate

--- src/DisplayState.purs
+++ src/DisplayState.purs
@@ -3,10 +3,18 @@ module DisplayState where
 import Prelude
 
 import Data.Array ((!!))
-import Data.Maybe (Maybe(..))
-import ParseMidi (IState, Note)
+import Data.Array as A
+import Data.Map as M
+import Data.Maybe (Maybe)
+import ParseMidi (IState, Note, BarNum)
 
-f :: IState -> {bar :: Int, track :: Int} -> Maybe (Array Note)
-f state r = do 
-        track <- state.musicTracks !! r.track
-        Nothing
+getBarNotes :: IState -> { bar :: BarNum, track :: Int } -> Maybe (Array Note)
+getBarNotes state r = do
+    track <- state.musicTracks !! r.track
+    span <- M.lookup r.bar state.timeM
+    pure $ A.filter
+        ( \x ->
+              if span.start <= x.on && span.end >= x.off then true
+              else false
+        )
+        track.noteEvents
